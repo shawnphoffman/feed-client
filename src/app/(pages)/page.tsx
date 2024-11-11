c7c7d2037861ee38ce2a59a62a2d64b3a5eb9107
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-11-11 13:22:40 -0800

    revalidate

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -3,6 +3,9 @@ import { notFound } from 'next/navigation'
 import { agent } from '@/api'
 import { BskyPost } from '@/components/bsky-post'
 
+// Every 15 minutes
+export const revalidate = 900
+
 export default async function Homepage() {
 	// await new Promise(resolve => setTimeout(resolve, 10000))
 	const { success, data } = await agent.app.bsky.feed.getFeed(
